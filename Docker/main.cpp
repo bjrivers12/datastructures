4282bce6a2222e4f429fbb454600e776f321cc18
bjrivers30 <79800631+bjrivers30@users.noreply.github.com>
2023-11-29 17:59:29 -0700

    initial commit

---
+++ Docker/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <memory>
+#include <cstdarg>
+
+using namespace std;
+
+class Rectangle
+{
+    int length;
+    int breadth;
+
+public:
+    Rectangle(int l, int b)
+    {
+        length = l;
+        breadth = b;
+    }
+    int area()
+    {
+        return length * breadth;
+    }
+};
+
+int sum(int n,...)
+{
+    va_list list;
+    va_start(list,n);
+    
+    int x;
+    int s=0;
+    for(int i=0;i<n;i++){
+        x=va_arg(list,int);
+        s+=x;
+    }
+    return s;
+}
+
+int main(int argc, char const *argv[])
+{
+
+cout<<sum(3,10,20,30)<<endl;
+cout<<sum(5,1,2,3,4,5)<<endl;
+}
